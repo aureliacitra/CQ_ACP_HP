b1d47d5909cf2322a1fbd9c953e99ca2ffcf118e
aureliacitra <citrapangukir@gmail.com>
2026-03-31 12:51:36 +0700

    add library

--- PraktikumADO/Form1.cs
+++ PraktikumADO/Form1.cs
@@ -10,7 +10,7 @@ using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.Xml.Serialization;
+using System.Xml.Serialization; //add library
 
 namespace PraktikumADO
 {
